299819d544d910049ec7ee8e2c11ee2aa00e3936
Nguyen Duc Vinh <vinhb2014631@student.ctu.edu.vn>
2023-04-01 16:13:31 +0700

    fix header

--- partials/header.php
+++ partials/header.php
@@ -9,10 +9,14 @@
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Bootstrap icons-->
+        
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="css/css_phone.css">
+        <script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
         <!-- Core theme CSS (includes Bootstrap)-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
         <link href="css/styles.css" rel="stylesheet" />
         
     </head>
@@ -35,23 +39,20 @@
                 <a class="navbar-brand" href="#"><img style="height: 72px;" src="./imgs/logo.png" alt=""></a>
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                   <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Trang Chủ</a>
+                    <a class="nav-link active fs-4" aria-current="page" href="#">Trang Chủ</a>
                   </li>
+                  <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle fs-4" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+           Điện thoại
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Iphone</a></li>
+            <li><a class="dropdown-item" href="#">SamSung</a></li>
+            <li><a class="dropdown-item" href="#">Các dòng khác</a></li>
+          </ul>
+        </li>
                   <li class="nav-item">
-                    <div class="dropdown show">
-                    <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown link
-                    </a>
-
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-</div>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Về chúng tôi</a>
+                    <a class="nav-link fs-4" href="#">Về chúng tôi</a>
                   </li>
                 </ul>
                 <form class="d-flex input-group w-auto">
