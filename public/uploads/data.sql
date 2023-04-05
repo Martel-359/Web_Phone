0239ffe1b57c0051fcce1d9cf922cf58d1d261d1
Martel-359 <doanhuuvinh0108@gmail.com>
2023-04-05 10:30:53 +0700

    commit database

--- public/uploads/data.sql
+++ public/uploads/data.sql
@@ -1,18 +1,17 @@
-CREATE TABLE `loai_hang_hoa` (
+CREATE TABLE `loai_hang_hoas` (
   `id` int(11) NOT NULL PRIMARY KEY,
   `Ten_loai` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO `loai_hang_hoa` (`id`, `Ten_loai`) VALUES
+INSERT INTO `loai_hang_hoas` (`id`, `Ten_loai`) VALUES
 (1, 'Laptop'),
 (2, 'SamSung'),
 (3, 'iPhone');
 
 
-CREATE TABLE `hang_hoa` (
+CREATE TABLE `hang_hoas` (
   `id` int(11) NOT NULL PRIMARY KEY,
-  `Ten_hang_hoa` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `name_search` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ten_hang_hoa` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `gia` int(11) NOT NULL,
   `so_luong_hang` int(11) NOT NULL,
   `hinh` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -22,19 +21,19 @@ CREATE TABLE `hang_hoa` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
-INSERT INTO `hang_hoa` (`id`, `Ten_hang_hoa`, `name_search`, `gia`, `so_luong_hang`, `hinh`, `mo_ta`, `id_loai`) VALUES
-(1, 'Điện thoại Samsung Galaxy Note 10+', 'Điện-thoại-Samsung-Galaxy-Note-10-', 15900000, 90, 'public/uploads/12333.png', 'mo_ta', 2),
-(2, 'Điện thoại Samsung Galaxy A21s (6GB/64GB)', 'Điện-thoại-Samsung-Galaxy-A21s-(6GB-64GB)', 52900000, 96, 'public/uploads/samsung-galaxy-a21s-055620-045627-400x460.png', 'mo_ta', 2),
-(3, 'Điện thoại Samsung Galaxy Note 20 Ultra', 'Điện-thoại-Samsung-Galaxy-Note-20-Ultra', 29900000, 97, 'public/uploads/samsung-galaxy-note-20-ultra-vangdong-400x460-400x460.png', 'mo_ta', 2),
-(4, 'Điện thoại Samsung Galaxy Z Flip', 'Điện-thoại-Samsung-Galaxy-Z-Flip', 36000000, 96, 'public/uploads/samsung-galaxy-z-flip-chitiet-2-788x544.png', 'mo_ta', 2),
-(5, 'Điện thoại Samsung Galaxy Z Fold2 5G', 'Điện-thoại-Samsung-Galaxy-20Z-Fold2-5G', 50000000, 97, 'public/uploads/samsung-galaxy-z-fold-2-123620-093621-400x460.png', 'mo_ta', 2),
-(6, 'Điện thoại iPhone 8 Plus 128GB', 'Điện-thoại-iPhone-8-Plus-128GB', 14900000, 99, 'public/uploads/ip8s.jpg', 'mo_ta', 3),
-(7, 'Điện thoại iPhone 7 Plus 128GB', 'Điện-thoại-iPhone-7-Plus-128GB', 11490000, 99, 'public/uploads/iphone-7-plus-128gb-de-400x460.png', 'mo_ta', 3),
-(8, 'Điện thoại iPhone SE 64GB (2020)', 'Điện-thoại-iPhone-SE-64GB-(2020)', 12900000, 49, 'public/uploads/iphone-se-2020-do-400x460-400x460.png', 'mo_ta', 3),
-(9, 'Điện thoại iPhone 11 Pro 256GB', 'Điện-thoại-iPhone-11-Pro-256GB', 31900000, 26, 'public/uploads/ip11pr.jpg', 'mo_ta', 3),
-(10, 'Laptop HP Envy 13 ba0046TU i5 1035G4/8GB/512GB/Office H&S2019/Win10 (171M7PA)', 'Laptop-HP-Envy-13-ba0046TU-i5-1035G4-8GB-512GB-Office-H-S2019-Win10-(171M7PA)', 22900000, 19, 'public/uploads/hp-envy-13-ba0046tu-i5-171m7pa-225859-600x600.jpg', 'mo_ta', 1),
-(11, 'Laptop Asus VivoBook Gaming F571GT i7 9750H/8GB/512GB/120Hz/4GB GTX1650/Win10 (AL858T)', 'Laptop-Asus-VivoBook-Gaming-F571GT-i7-9750H-8GB-512GB-120Hz-4GB-GTX1650-Win10-(AL858T)', 24490000, 18, 'public/uploads/asus-vivobook-gaming-f571gt-i7-al858t-226256-600x600.jpg', 'mo_ta', 1),
-(12, 'Laptop Lenovo IdeaPad Slim 3 15IIL05 i3 1005G1/4GB/512GB/Win10 (81WE003RVN)', 'Laptop-Lenovo-IdeaPad-Slim-3-15IIL05-i3-1005G1-4GB-512GB-Win10-(81WE003RVN)', 25000000, 5, 'public/uploads/lenovo-ideapad-3-15iil05-i3-81we003rvn-013920-053901-600x600.jpg', 'mo_ta',1);
+INSERT INTO `hang_hoas` (`id`, `ten_hang_hoa`, `gia`, `so_luong_hang`, `hinh`, `mo_ta`, `id_loai`) VALUES
+(1, 'Điện thoại Samsung Galaxy Note 10+',15900000, 90, '12333.png', 'mo_ta', 2),
+(2, 'Điện thoại Samsung Galaxy A21s (6GB/64GB)', 52900000, 96, 'samsung-galaxy-a21s-055620-045627-400x460.png', 'mo_ta', 2),
+(3, 'Điện thoại Samsung Galaxy Note 20 Ultra', 29900000, 97, 'samsung-galaxy-note-20-ultra-vangdong-400x460-400x460.png', 'mo_ta', 2),
+(4, 'Điện thoại Samsung Galaxy Z Flip', 36000000, 96, 'samsung-galaxy-z-flip-chitiet-2-788x544.png', 'mo_ta', 2),
+(5, 'Điện thoại Samsung Galaxy Z Fold2 5G', 50000000, 97, 'samsung-galaxy-z-fold-2-123620-093621-400x460.png', 'mo_ta', 2),
+(6, 'Điện thoại iPhone 8 Plus 128GB', 14900000, 99, 'ip8s.jpg', 'mo_ta', 3),
+(7, 'Điện thoại iPhone 7 Plus 128GB', 11490000, 99, 'iphone-7-plus-128gb-de-400x460.png', 'mo_ta', 3),
+(8, 'Điện thoại iPhone SE 64GB (2020)', 12900000, 49, 'iphone-se-2020-do-400x460-400x460.png', 'mo_ta', 3),
+(9, 'Điện thoại iPhone 11 Pro 256GB', 31900000, 26, 'ip11pr.jpg', 'mo_ta', 3),
+(10, 'Laptop HP Envy 13 ba0046TU i5 1035G4/8GB/512GB/Office H&S2019/Win10 (171M7PA)', 22900000, 19, 'hp-envy-13-ba0046tu-i5-171m7pa-225859-600x600.jpg', 'mo_ta', 1),
+(11, 'Laptop Asus VivoBook Gaming F571GT i7 9750H/8GB/512GB/120Hz/4GB GTX1650/Win10 (AL858T)', 24490000, 18, 'asus-vivobook-gaming-f571gt-i7-al858t-226256-600x600.jpg', 'mo_ta', 1),
+(12, 'Laptop Lenovo IdeaPad Slim 3 15IIL05 i3 1005G1/4GB/512GB/Win10 (81WE003RVN)', 25000000, 5, 'lenovo-ideapad-3-15iil05-i3-81we003rvn-013920-053901-600x600.jpg', 'mo_ta',1);
 
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL PRIMARY KEY,
